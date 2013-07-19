8fbbf540d45af3e129f41ea919f4edd8140215c7
Packetfahrer <packetfahrer@web.de>
2013-07-19 14:54:42 +0200

    Test

---
+++ test1/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  test1
+//
+//  Copyright (c) 2013 GlobalReach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ test1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  test1
+//
+//  Copyright (c) 2013 GlobalReach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <iAd/iAd.h>
+
+@interface ViewController : UIViewController <ADBannerViewDelegate>
+
+@end
